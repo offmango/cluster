9553a6373a41125eaa6d3067c9859fa87a2ebd08
offmango <offmango@gmail.com>
2012-11-12 14:12:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cluster::Application.config.secret_token = 'bdb23d74c614e8989de7748c7886f8f2a01fbddebc5dcf44b0119d7652fc8f8674d6eeb8fee2dddf4134b6a495ae8a2f0ba2f56df26a269347b751baaec4e88e'
